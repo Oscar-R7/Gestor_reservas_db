b43d3f5bee6ceef374e2d8eb03c697640748cae1
sevenstark <7s.nation1@gmail.com>
2026-04-15 19:59:59 -0500

    guardando cambios

--- app.py
+++ app.py
@@ -71,11 +71,8 @@ def confirmar_reserva():
             session['usuario_email']=email
             
             flash('Reserva confirmada')
-<<<<<<< HEAD
-        except as e:
-=======
-        Exception as e:
->>>>>>> master
+
+        except Exceptiona as e:    
             print(f"Error: {e}")
             mysql.connection.rollback()
         finally:
